91eb44a664b073b07d2353087788746ff7de23d2
Shudhanshu Kumar <145117356+Shudha18@users.noreply.github.com>
2026-06-09 11:27:27 +0530

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int carry = 1;
+        int size = digits.size();
+
+        for(int i=size-1; i>=0; i--){
+            int sum = digits[i] + carry;
+            digits[i] = sum%10;
+            carry = sum/10;
+
+            if(carry == 0) break;
+        }
+        if(carry == 1){
+            digits.insert(digits.begin(), 1);
+        }
+        return digits;
+    }
+};
