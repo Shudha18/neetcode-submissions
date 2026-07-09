3bc39d34c00d1ed73ca8e7cc665cb8c2208da318
Shudhanshu Kumar <145117356+Shudha18@users.noreply.github.com>
2026-07-09 12:08:05 +0530

    Add: move-zeroes - submission-0

---
+++ Data Structures & Algorithms/move-zeroes/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int zero = 0;
+        for(int i : nums){
+            if(i == 0) zero++;
+        }
+        nums.erase(remove(nums.begin(), nums.end(), 0), nums.end());
+        for(int i=0; i<zero; i++){
+            nums.push_back(0);
+        }
+    }
+};
